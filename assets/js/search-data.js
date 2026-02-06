525ef67a478f34b7c31edcaa30a22c6a7845f725
Long19950304 <yb87315@um.edu.mo>
2026-02-06 03:57:54 +0000

    Deploying to gh-pages from @ Long19950304/Long19950304.github.io@de2bd5cd29f4d8af6a9e7cdca1b4ca9d997726ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -159,6 +159,11 @@ ninja.data = [{
           description: "",
           section: "Digests",handler: () => {
               window.location.href = "/digest/2026-02-05/";
+            },},{id: "digests-daily-digest-2026-02-06",
+          title: 'Daily Digest - 2026-02-06',
+          description: "",
+          section: "Digests",handler: () => {
+              window.location.href = "/digest/2026-02-06/";
             },},{id: "digests_zh-每日简报-2026-01-20",
           title: '每日简报 — 2026-01-20',
           description: "",
@@ -239,6 +244,11 @@ ninja.data = [{
           description: "",
           section: "Digests_zh",handler: () => {
               window.location.href = "/zh/digest/2026-02-05/";
+            },},{id: "digests_zh-每日简报-2026-02-06",
+          title: '每日简报 - 2026-02-06',
+          description: "",
+          section: "Digests_zh",handler: () => {
+              window.location.href = "/zh/digest/2026-02-06/";
             },},{id: "projects-agentic-engineering-ae-x",
           title: 'Agentic Engineering (AE+X)',
           description: "A unifying research agenda for configuring, testing, and deploying multi-agent systems for reliable outcomes.",

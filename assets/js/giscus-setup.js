9b4fa34637cb54228b07748ad233fb7626183a8e
Long19950304 <yb87315@um.edu.mo>
2026-04-23 03:33:02 +0000

    Deploying to gh-pages from @ Long19950304/Long19950304.github.io@11afb3544fe4c390c048024172ad05f879d6f7b0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "Long19950304/Long19950304.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",

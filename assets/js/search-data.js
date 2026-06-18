c160f526fd078a08d2182a51c969d3988b8b8560
Long19950304 <yb87315@um.edu.mo>
2026-06-18 03:56:11 +0000

    Deploying to gh-pages from @ Long19950304/Long19950304.github.io@20abfe10c0cb38c698beff60e6d216af517b06d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -234,6 +234,16 @@ ninja.data = [{
           description: "",
           section: "Digests",handler: () => {
               window.location.href = "/digest/2026-06-16/";
+            },},{id: "digests-daily-digest-2026-06-17",
+          title: 'Daily Digest - 2026-06-17',
+          description: "",
+          section: "Digests",handler: () => {
+              window.location.href = "/digest/2026-06-17/";
+            },},{id: "digests-daily-digest-2026-06-18",
+          title: 'Daily Digest - 2026-06-18',
+          description: "",
+          section: "Digests",handler: () => {
+              window.location.href = "/digest/2026-06-18/";
             },},{id: "digests_zh-每日简报-2026-01-20",
           title: '每日简报 — 2026-01-20',
           description: "",
@@ -389,6 +399,16 @@ ninja.data = [{
           description: "",
           section: "Digests_zh",handler: () => {
               window.location.href = "/zh/digest/2026-06-16/";
+            },},{id: "digests_zh-每日简报-2026-06-17",
+          title: '每日简报 - 2026-06-17',
+          description: "",
+          section: "Digests_zh",handler: () => {
+              window.location.href = "/zh/digest/2026-06-17/";
+            },},{id: "digests_zh-每日简报-2026-06-18",
+          title: '每日简报 - 2026-06-18',
+          description: "",
+          section: "Digests_zh",handler: () => {
+              window.location.href = "/zh/digest/2026-06-18/";
             },},{id: "projects-agentic-engineering-ae-x",
           title: 'Agentic Engineering (AE+X)',
           description: "A unifying research agenda for configuring, testing, and deploying multi-agent systems for reliable outcomes.",
